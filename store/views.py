23ccc4eb68a970280a31a7051bae8f1cd6cfade1
Thomasbsgr <thomasbsgr@gmail.com>
2025-05-23 21:43:57 +0200

    first

---
+++ store/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, get_object_or_404
+from store.models import Product
+from django.http import HttpResponse
+
+def index(request):
+    products = Product.objects.all()
+
+    return render(request, 'store/index.html', context={"products": products})
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    return render(request, 'store/detail.html', context={"product": product})
